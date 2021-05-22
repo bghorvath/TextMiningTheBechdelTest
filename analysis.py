926ad3a76b9ef335b00a92d87cb0aaa3ec7affba
bghorvath <balazsghorvath@gmail.com>
2021-05-22 23:07:48 +0200

    Dialogues added

--- analysis.py
+++ analysis.py
@@ -127,9 +127,7 @@ for root, _, files in os.walk(input_dir):
 script_text_split = filter(None.__ne__, script_text_split)
 script_text_split = list(script_text_split)
 
-len(script_text_split)
-
-# %%
+## Assigning headers
 
 script_text_ind = []
 
@@ -139,9 +137,7 @@ for i in script_text_split:
     else:
         script_text_ind.append((i, 'text'))
 
-# script_text_ind[1]
-
-# %%
+## Matching headings with texts
 
 old_block = ('init', 'text')
 
@@ -162,25 +158,68 @@ len(paragraphs)
 
 # %%
 
-paragraphs_df = pd.DataFrame(paragraphs, columns = ['heading', 'text'])
+# paragraphs_df = pd.DataFrame(paragraphs, columns = ['heading', 'text'])
 
-print(paragraphs_df[paragraphs_df['heading'] == 'NA'])
-print(paragraphs_df[paragraphs_df['text'] == 'NA'])
+# print(paragraphs_df[paragraphs_df['heading'] == 'NA'])
+# print(paragraphs_df[paragraphs_df['text'] == 'NA'])
 
-paragraphs_df
+# paragraphs_df
+
+# # %%
+
+# pg = paragraphs[30]
+# pg
 
 # %%
+## Going through paragraphs, collecting person names
 
-pg = paragraphs_df.iloc[30,1]
-pg
+char_pattern = r"[^a-zA-Z \n](?:[ ]|\n)*\n[ ]*((?:[A-Z']+(?:[ ][A-Z]+)?)|(?:[A-Z]+[. ]){1,3})\s*\n"
 
-# %%
+char_set = set()
 
-char_pattern = r"\n?\s*([A-Z]+|(?:[A-Z]+\.){1,3})\s*\n"
+for pg in paragraphs:
+    char_set.update(set(re.findall(char_pattern, pg[1])))
 
-char_split = re.split(char_pattern, pg)
+dialogues = []
 
-char_split
+for pg in paragraphs:
+    char_split = re.split(char_pattern, pg[1])
+    
+    char_split_ind = []
+
+    for i in char_split:
+        if i in char_set:
+            char_split_ind.append((i, 'char'))
+        else:
+            char_split_ind.append((i, 'text'))
+
+    clean_char_split_ind = []
+    
+    for i in char_split_ind:
+        if i[1] == 'char':
+            clean_char_split_ind.append(i)
+        if i[1] == 'text':
+            line = i[0]
+            clean_line = re.sub(r"[^a-zA-Z0-9.\-,;:!?()'\"\s]", "", line)
+            clean_line = re.sub(r"\s+"," ", clean_line)
+            clean_char_split_ind.append((clean_line, 'text'))
+            
+
+    old_block = ('init', 'text')
+    # dialogues = []
+
+    for block in clean_char_split_ind:
+
+        if block[1] == 'text' and old_block[1] == 'char':
+            dialogues.append((old_block[0], block[0]))
+        elif block[1] == 'text' and old_block[1] == 'text':
+            dialogues.append(('NA', block[0]))
+        elif block[1] == 'char' and old_block[1] == 'char':
+            dialogues.append((old_block[0], 'NA'))
+
+        old_block = block
+    
+print(len(dialogues))
 
 # %%
 
@@ -205,7 +244,7 @@ nlp = spacy.load("en_core_web_trf")
 
 char_pattern = r"(\n?\s*(?:[A-Z]+|(?:[A-Z]+\.){1,3}))\s*\n"
 
-pg = "Stacy's mom is coming over the weekend to see her daughter."
+#pg = "Stacy's mom is coming over the weekend to see her daughter."
 
 pg_clean = re.sub(char_pattern,r"\1:",pg)
 pg_clean = re.sub(r"[^a-zA-Z0-9.\-,;:!?()'\"\s]","",pg_clean)
@@ -224,15 +263,17 @@ doc = nlp(pg_clean)
 
 ner_persons = set()
 
-for ent in doc.ents:
-    print(ent.text)
-    if ent.label_ == 'PERSON' and ent.text not in ner_persons:
-        ner_persons.add(ent.lemma_)
+# for ent in doc.ents:
+#     print(ent.text)
+#     if ent.label_ == 'PERSON':
+#         ner_persons.add(ent.lemma_)
 
-ner_persons
-# for token in doc:
-#     a = [token.text, token.lemma_, token.pos_, token.tag_, token.dep_]
-#     a_list.append(a)
+# ner_persons
+a_set = set()
+for token in doc:
+    if token.tag_ == 'NNP':
+        a_set.add(token.text)
+print(a_set)
 
 
 # %%
@@ -250,3 +291,13 @@ displacy.render(doc, style="dep")
 
 # %%
 
+## Need to NER tagtog - do we? It's not ML after all - ask Kata
+
+## Next up: assign passive / active sentences
+    ## After that
+    #   Name normalization
+    #   Gender recognition
+    #   Who they talk to - next person in line talking
+    #   Coreferences inside text?
+    #   What they are talking about
+        # Maybe topic modelling?
